4487f73ff554f7bf3f5cfae712e7fae099bf7ce1
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-15 15:19:44 +0100

    installed axios with call

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,21 @@
 </template>
 
 <script>
-
+import axios from "axios"
 
 export default {
   name:"App",
   components: {
     
+  },
+  mounted(){
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then(r=>{
+      console.log(r);
+    }).catch(e=>{
+      console.log(e,"Ops")
+    })
   }
 }
 </script>
@@ -36,7 +45,7 @@ export default {
 <style lang="scss">
 
 @import "../node_modules/bootstrap/scss/bootstrap.scss";
-  
+
 body {
   font-family: "Montserrat"; 
 }
